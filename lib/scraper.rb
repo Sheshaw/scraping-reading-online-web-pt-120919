c45730507627d6921c2237ce2f9ddd6ebbba5f2c
Chayan Martinez <nayachmartinez68@gmail.com>
2020-02-05 15:16:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,5 +6,5 @@ html = open("https://flatironschool.com/")
 Nokogiri::HTML(html)
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-260IBN")
+doc.css(".headline-260IBN").text
 
